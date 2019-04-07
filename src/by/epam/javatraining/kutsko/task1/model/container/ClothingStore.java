e91bfb6593ac9180c3ee9fbd22e4ba6c9678504b
katekutsko <47540395+katekutsko@users.noreply.github.com>
2019-04-07 20:28:50 +0300

    Added annotation for JAXB Parser

--- src/by/epam/javatraining/kutsko/task1/model/container/ClothingStore.java
+++ src/by/epam/javatraining/kutsko/task1/model/container/ClothingStore.java
@@ -1,38 +1,51 @@
 package by.epam.javatraining.kutsko.task1.model.container;
 
 import java.io.Serializable;
+
 import java.util.Arrays;
-import java.util.Objects;
+import java.util.Collection;
+
+import javax.xml.bind.annotation.*;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-import by.epam.javatraining.kutsko.task1.model.entity.Item;
+import by.epam.javatraining.kutsko.task1.model.entity.*;
 import by.epam.javatraining.kutsko.task1.model.exception.CorruptItemReferenceException;
 import by.epam.javatraining.kutsko.task1.model.exception.NoSuchItemException;
 import by.epam.javatraining.kutsko.task1.model.exception.WarehouseFullException;
+
+@XmlRootElement(name = "store", namespace = "http://example.com/clothing")
 /**
- * Container class representing a clothing store. 
+ * Container class representing a clothing store.
  * 
  * @author Екатерина
  *
  */
 public class ClothingStore implements Serializable {
-
+	@XmlTransient
 	private static final Logger LOGGER;
 
 	static {
 		LOGGER = Logger.getRootLogger();
-		PropertyConfigurator.configure("log4j.properties");
+		PropertyConfigurator.configure("resource/log4j.xml");
 	}
-
+	@XmlTransient
 	public final static int MAX_CAPACITY = 100;
+	@XmlTransient
 	public final static int DEFAULT_CAPACITY = 10;
-	private final static String HEADER = "Contents of clothing store warehouse:\n";
-		
+	@XmlTransient
+	private final static String HEADER = "Contents of clothing store warehouse:";
+	@XmlTransient
 	private int currentAmountOfProducts = 0;
-	private Item[] itemSet;
+	@XmlTransient
 	private boolean sorted;
+
+	@XmlElements(value = { @XmlElement(name = "scarf", type = Scarf.class),
+			@XmlElement(name = "jumper", type = Jumper.class),
+			@XmlElement(name = "highheels", type = HighHeels.class) })
+	private Item[] itemSet;
+
 	/**
 	 * Default constructor. Initializes item set as an array of default capacity
 	 */
@@ -41,16 +54,35 @@ public class ClothingStore implements Serializable {
 	}
 
 	/**
-	 * Parameterized constructor. Initializes item set as an array of specified capacity
+	 * Parameterized constructor. Initializes item set as an array of specified
+	 * capacity
+	 * 
 	 * @param capacity size of an item set array
 	 */
 	public ClothingStore(int capacity) {
 		itemSet = new Item[capacity];
 	}
 
+	public ClothingStore(Collection<Item> items) {
+
+		itemSet = new Item[items.size()];
+
+		for (Item item : items) {
+			try {
+				addProduct(item);
+			} catch (WarehouseFullException e) {
+				LOGGER.info("Store capacity reached");
+				break;
+			} catch (CorruptItemReferenceException e) {
+				LOGGER.info("Item was null");
+			}
+		}
+	}
+
 	/**
-	 * Copying constructor. Copies item set from a template container if latter is not null, otherwise
-	 * calls for default constructor
+	 * Copying constructor. Copies item set from a template container if latter is
+	 * not null, otherwise calls for default constructor
+	 * 
 	 * @param store template to copy
 	 */
 	public ClothingStore(ClothingStore store) {
@@ -65,16 +97,16 @@ public class ClothingStore implements Serializable {
 					LOGGER.info("Item in the template storage was null");
 				}
 			}
-		}
-		else {
+		} else {
 			LOGGER.warn("Reference to a template object was null");
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param index index of a item to get
-	 * @return item under specified index or null if such index is not occupied, but allowed
+	 * @return item under specified index or null if such index is not occupied, but
+	 *         allowed
 	 * @throws NoSuchItemException when index surpasses allowed values
 	 */
 	public Item getItem(int index) throws NoSuchItemException {
@@ -87,7 +119,7 @@ public class ClothingStore implements Serializable {
 			throw new NoSuchItemException("There is no element with index " + index);
 		}
 	}
-	
+
 	/**
 	 * @return current item set
 	 */
@@ -96,10 +128,10 @@ public class ClothingStore implements Serializable {
 		return itemSet;
 	}
 
-	
 	/**
 	 * 
-	 * @return indicator which shows if item set is sorted by price. Useful for binary search by price
+	 * @return indicator which shows if item set is sorted by price. Useful for
+	 *         binary search by price
 	 */
 	public boolean isSorted() {
 		return sorted;
@@ -120,7 +152,7 @@ public class ClothingStore implements Serializable {
 	public int getCurrentAmountOfProducts() {
 		return currentAmountOfProducts;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -139,9 +171,9 @@ public class ClothingStore implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		
+
 		ClothingStore other = (ClothingStore) obj;
-		
+
 		if (currentAmountOfProducts != other.currentAmountOfProducts)
 			return false;
 		if (!Arrays.equals(itemSet, other.itemSet))
@@ -150,12 +182,13 @@ public class ClothingStore implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 	/**
 	 * 
 	 * @param item item to add to the storage
-	 * @throws WarehouseFullException when maximum capacity of the storage is reached
-	 * @throws CorruptItemReferenceException  when item reference is null
+	 * @throws WarehouseFullException        when maximum capacity of the storage is
+	 *                                       reached
+	 * @throws CorruptItemReferenceException when item reference is null
 	 */
 	public void addProduct(Item item) throws WarehouseFullException, CorruptItemReferenceException {
 		if (item != null) {
@@ -168,8 +201,9 @@ public class ClothingStore implements Serializable {
 				throw new WarehouseFullException(
 						"You can't add more than " + MAX_CAPACITY + " item into the warehouse");
 			}
-		} 
-		throw new CorruptItemReferenceException("Item reference was null");
+		} else {
+			throw new CorruptItemReferenceException("Item reference was null");
+		}
 	}
 
 	/**
@@ -179,11 +213,12 @@ public class ClothingStore implements Serializable {
 		itemSet = Arrays.copyOf(itemSet, currentAmountOfProducts + 1);
 	}
 
-	
 	/**
 	 * removes item under specified index
+	 * 
 	 * @param index index of the product to be removed
-	 * @return true if item was deleted, false if index pointed beyond the boundaries of item set
+	 * @return true if item was deleted, false if index pointed beyond the
+	 *         boundaries of item set
 	 */
 	public boolean removeProduct(int index) {
 		if (index >= 0 && index < currentAmountOfProducts) {
@@ -203,6 +238,7 @@ public class ClothingStore implements Serializable {
 			return false;
 		}
 	}
+
 	/**
 	 * removes all items from item set
 	 */
@@ -222,7 +258,6 @@ public class ClothingStore implements Serializable {
 			builder.append(HEADER);
 			for (int i = 0; i < getCurrentAmountOfProducts(); i++) {
 				builder.append(itemSet[i].toString());
-				builder.append("\n");
 			}
 			return builder.toString();
 		} else {
